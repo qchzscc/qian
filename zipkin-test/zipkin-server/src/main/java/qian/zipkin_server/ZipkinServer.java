f6459e4abe55f7a5c4219d4d798388c8239796fe
chenqian2@creditease.cn <chenqian2@creditease.cn>
2017-08-04 18:44:22 +0800

    change

--- zipkin-test/zipkin-server/src/main/java/qian/zipkin_server/ZipkinServer.java
+++ zipkin-test/zipkin-server/src/main/java/qian/zipkin_server/ZipkinServer.java
@@ -2,11 +2,10 @@ package qian.zipkin_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import zipkin.server.EnableZipkinServer;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 @EnableZipkinServer
 public class ZipkinServer {
